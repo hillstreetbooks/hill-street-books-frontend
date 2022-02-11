b7b7fff9c62e304d678af564c727edf84312929a
Gokul <gokulpulari@gmail.com>
2022-02-11 16:10:49 -0500

    Created a reusable FormContent component

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
 import Button from './Button/Button';
 import Footer from './Shared/Footer/Footer';
 import Form from './Form/Form';
+import FormContent from './Form/FormContent';
 import Header from './Shared/Header/Header';
 import Image from './Image/Image';
 import Input from './Input/Input';
 
-export { Button, Footer, Form, Header, Image, Input };
+export { Button, Footer, Form, FormContent, Header, Image, Input };
