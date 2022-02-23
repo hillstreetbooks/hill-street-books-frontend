f6d33aafd4b63d366b91592535149bd02148c2fe
Gokul <gokulpulari@gmail.com>
2022-02-23 14:36:00 -0500

    [HSB-63] Forgot Password Page implementation

---
+++ src/views/Password/ResetPassword.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Form, FormContent, Loader, PopupModal } from '../../components';
+import { RESET_PASSWORD } from '../../constants/Strings';
+import ForgotPasswordBanner from '../../assets/library.jpeg';
+import { useForm, useModal } from '../../hooks';
+import { AuthorService } from '../../services';
+import './ForgotPassword.scss';
+
+const ResetPassword = () => {
+  const [errors, setErrors] = useState({});
+  const { userId, uniqueString } = useParams();
+  const [isLoading, updateLoader] = useState(false);
+
+  const resetPassword = () => {
+    const { password, confirm_password } = inputs;
+    let validationResult = validate();
+    setErrors(validationResult);
+    if (validationResult && Object.keys(validationResult).length === 0) {
+      updateLoader(true);
+      AuthorService.resetPassword(
+        userId,
+        uniqueString,
+        password,
+        confirm_password
+      )
+        .then((response) => {
+          toggleVisibility(!show);
+          setModalMessage(
+            response ||
+              'Oops, Something went wrong! Please contact the administrator.'
+          );
+          updateLoader(false);
+        })
+        .catch((error) => {
+          toggleVisibility(!show);
+          setModalMessage(
+            'Oops, Something went wrong! Please contact the administrator.'
+          );
+          console.log(error);
+          updateLoader(false);
+        });
+    }
+  };
+
+  const { show, toggleVisibility, modalMessage, setModalMessage } = useModal();
+
+  const { inputs, handleInputChange, handleSubmit, validate } = useForm(
+    RESET_PASSWORD.FIELDS,
+    resetPassword
+  );
+
+  return (
+    <div className="forgot-password-wrapper">
+      <Form
+        image={ForgotPasswordBanner}
+        content={RESET_PASSWORD.CONTENT}
+        handleSubmit={handleSubmit}
+      >
+        <FormContent
+          fields={RESET_PASSWORD.CONTENT.fields}
+          values={inputs}
+          errors={errors}
+          handleInputChange={handleInputChange}
+          buttonText="Reset Password"
+        ></FormContent>
+      </Form>
+      <PopupModal
+        show={show}
+        toggleVisibility={() => toggleVisibility(!show)}
+        message={modalMessage}
+      />
+      {isLoading ? <Loader /> : null}
+    </div>
+  );
+};
+
+export default ResetPassword;
--- src/views/index.js
+++ src/views/index.js
@@ -1,5 +1,7 @@
 import Error from './Error/Error';
+import ForgotPassword from './Password/ForgotPassword';
 import Login from './Login/Login';
 import Registration from './Registration/Registration';
+import ResetPassword from './Password/ResetPassword';
 
-export { Error, Login, Registration };
+export { Error, ForgotPassword, Login, Registration, ResetPassword };
