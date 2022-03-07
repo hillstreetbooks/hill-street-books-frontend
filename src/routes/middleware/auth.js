c3538aa49271b9b8f5d2310902752a8c904da464
Gokul <gokulpulari@gmail.com>
2022-03-06 20:28:19 -0500

    Updated protected routes

--- src/routes/middleware/auth.js
+++ src/routes/middleware/auth.js
@@ -1,10 +1,12 @@
-import { Navigate, useLocation } from 'react-router-dom';
-import { isLoggedIn } from '../../store';
+import { Navigate, useLocation, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const RequireAuth = ({ children }) => {
-  let location = useLocation();
+  const location = useLocation();
+  const { _id } = useSelector((state) => state.user.info);
+  let { authorId } = useParams();
 
-  if (isLoggedIn()) {
+  if (authorId && authorId !== _id) {
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
